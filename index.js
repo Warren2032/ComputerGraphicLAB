b37bfd385bfb9b65c6a537e4789c71fef5469dcb
Warren2032 <115253700+Warren2032@users.noreply.github.com>
2023-01-17 19:04:45 +0700

    1-4 Done
    5 in progress

--- index.js
+++ index.js
@@ -1,115 +1,138 @@
 import * as THREE from "./three.js/build/three.module.js";
+import { OrbitControls } from "./three.js/examples/jsm/controls/OrbitControls.js";
 
-let scene, camera, renderer;
+let scene, camera1, camera2, renderer, control, currentCamera, anim;
 
+// function skyBox(){
+//   let box = new THREE.BoxGeometry(1000,1000,1000);
+//   let loader = new THREE.TextureLoader()
+//   let boxMat = [
+//     new THREE.MeshBasicMaterial({
+//       map : loader.load('./assets/skybox/dawn_back.png')
+//     })
+//   ]
+// }
+
+function debugBox() {
+  let box = new THREE.Mesh(
+    new THREE.BoxGeometry(50, 50, 50),
+    new THREE.MeshBasicMaterial({color:"#ff0000"}));
+
+  box.position.set(0, 50, 0);
+  box.castShadow = true;
+
+  scene.add(box);
+}
+
+//lighting 1(ambient), 2,3,4 (spotlight)
+function initLight(){
+  createLight();
+  createLight1();
+  createLight2();
+  createLight3();
+}
 function createLight() {
-  let light = new THREE.SpotLight("#ffffff", 1);
-  light.position.set(100, 200, 300);
+  let light = new THREE.AmbientLight("#404040");
+
+  scene.add(light);
+}
+function createLight1() {
+  let light = new THREE.SpotLight("#ffffff", 1, 300);
+  light.position.set(-100, 0, 100);
+  light.lookAt(0,50,0);
+  light.castShadow = true;
+
+  scene.add(light);
+}
+function createLight2() {
+  let light = new THREE.SpotLight("#ffffff", 1, 300);
+  light.position.set(-100, 0, -100);
+  light.lookAt(0,50,0);
+  light.castShadow = true;
+
+  scene.add(light);
+}
+function createLight3() {
+  let light = new THREE.SpotLight("#ffffff", 0.5, 300, Math.PI/4 + Math.PI/6);
+  light.position.set(0, 200, 0);
+  light.lookAt(0,0,0);
   light.castShadow = true;
 
   scene.add(light);
 }
 
-function createFloor() {
-  let floor = new THREE.Mesh(
-    new THREE.BoxGeometry(250, 2, 150),
-    new THREE.MeshPhongMaterial({
-      map: new THREE.TextureLoader().load("./assets/floor.png"),
-    })
-  );
-  floor.position.set(35, -14, 35);
-  floor.rotation.set(0, (Math.PI / 4) * -1, 0);
-  floor.receiveShadow = true;
-
-  scene.add(floor);
-}
-
-function createPin() {
-  //upper
-  let upper = new THREE.Mesh(
-    new THREE.CylinderGeometry(3, 8, 20, 12, 12),
-    new THREE.MeshPhongMaterial({ color: "#ffffff" })
-  );
-  upper.position.set(0, 9, 0);
-  upper.castShadow = true;
-
-  scene.add(upper);
-
-  //lower
-  let lower = new THREE.Mesh(
-    new THREE.CylinderGeometry(8, 5, 12, 12, 12),
-    new THREE.MeshPhongMaterial({ color: "#ffffff" })
-  );
-  lower.position.set(0, -7, 0);
-  lower.castShadow = true;
-
-  scene.add(lower);
-
-  //neck
-  let neck = new THREE.Mesh(
-    new THREE.CylinderGeometry(4, 3, 12, 12, 12),
-    new THREE.MeshPhongMaterial({ color: "#ffffff" })
-  );
-  neck.position.set(0, 23, 0);
-  neck.castShadow = true;
-
-  scene.add(neck);
-
-  //head
-  let head = new THREE.Mesh(
-    new THREE.SphereGeometry(4, 32, 16),
-    new THREE.MeshPhongMaterial({ color: "#ff0000" })
-  );
-  head.position.set(0, 25, 0);
-  head.castShadow = true;
-
-  scene.add(head);
-}
-
-function createBall() {
-  let ball = new THREE.Mesh(
-    new THREE.SphereGeometry(10, 32, 16),
-    new THREE.MeshPhongMaterial({
-      map: new THREE.TextureLoader().load("./assets/bowling-ball.png"),
-    })
-  );
-  ball.position.set(90, -3, 90);
-  ball.rotation.set(Math.PI / 2, Math.PI / 2, Math.PI / 2);
-  ball.castShadow = true;
-
-  scene.add(ball);
-}
-
 function init() {
   scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(
-    75,
+  //Camera 1
+  camera1 = new THREE.PerspectiveCamera(
+    50,
     window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+    1,
+    5000
   );
-  camera.position.set(20, 60, 180);
+  camera1.position.set(-180, 30, 0);
+  camera1.lookAt(0, 30, 0);
 
-  //tidak diminta tetapi agar tampilan mirip seperti contoh
-  camera.lookAt(0, 0, 0);
+  //Camera 2
+  camera2 = new THREE.PerspectiveCamera(
+    50,
+    window.innerWidth / window.innerHeight,
+    1,
+    5000
+  );
+  camera2.position.set(-200, 50, 0);
+  camera2.lookAt(0, 0, 0);
+
+  currentCamera=camera1;
 
   renderer = new THREE.WebGLRenderer({ antialiasing: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
-
-  createLight();
-  createFloor();
-  createPin();
-  createBall();
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFShadowMap;
 
   document.body.appendChild(renderer.domElement);
+ // control = new OrbitControls(currentCamera, renderer.domElement);
+
+  initLight();
+  //debugBox();
 }
+
 function render() {
-  requestAnimationFrame(render);
-  renderer.render(scene, camera);
+  anim = requestAnimationFrame(render);
+  //control.update();
+  renderer.render(scene, currentCamera);
 }
 
 window.onload = function () {
   init();
   render();
+  addEventListener();
 };
+
+window.onresize = function (){
+  currentCamera.aspect = window.innerWidth/window.innerHeight;
+  currentCamera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function addEventListener() {
+  document.addEventListener("keydown", keyboardListener);
+}
+
+function keyboardListener(e) {
+  let keyCode = event.keyCode;
+  //console.log(keyCode);
+  if(keyCode == 32) {
+    if (currentCamera == camera1) {
+      currentCamera = camera2;
+      control = new OrbitControls(currentCamera, renderer.domElement);
+
+      //this thing work?
+      // cancelAnimationFrame(anim);
+    } else {
+      currentCamera = camera1;
+    }
+  }
+}
